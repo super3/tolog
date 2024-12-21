90af3766d5ed3cccc7f3d342e6a925b273f42466
Shawn Wilkinson <me@super3.org>
2024-12-21 13:42:48 -0500

    Add Settings Sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,43 @@
 <template>
   <v-app>
     <v-main style="overflow-y: auto !important;">
-      <div class="settings-icon">
+      <div class="settings-icon" @click="toggleSettings">
         <v-icon icon="mdi-cog" size="16"></v-icon>
       </div>
+      
+      <!-- Settings Sidebar -->
+      <v-navigation-drawer
+        v-model="showSettings"
+        location="right"
+        temporary
+        width="300"
+      >
+        <v-list>
+          <v-list-item>
+            <v-list-item-title>Settings</v-list-item-title>
+            <template v-slot:append>
+              <v-btn
+                icon="mdi-close"
+                variant="text"
+                size="small"
+                @click="showSettings = false"
+              ></v-btn>
+            </template>
+          </v-list-item>
+          
+          <v-divider></v-divider>
+          
+          <!-- Add your settings options here -->
+          <v-list-item>
+            <v-list-item-title>Theme</v-list-item-title>
+          </v-list-item>
+          
+          <v-list-item>
+            <v-list-item-title>Font Size</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+
       <router-view></router-view>
     </v-main>
   </v-app>
@@ -11,7 +45,17 @@
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data() {
+    return {
+      showSettings: false
+    }
+  },
+  methods: {
+    toggleSettings() {
+      this.showSettings = !this.showSettings
+    }
+  }
 }
 </script>
 
@@ -53,5 +97,9 @@ html, body {
 
 .v-main::-webkit-scrollbar-thumb:hover {
   background: #555;
+}
+
+.v-navigation-drawer {
+  z-index: 1000;
 }
 </style> 
